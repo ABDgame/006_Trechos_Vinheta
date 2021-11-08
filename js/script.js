1dac3373e060071e5465c34e3cfcafec3650470c
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2021-11-08 12:01:19 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 
+const bg = document.getElementsByClassName('bg')[0];
+const blocks = document.getElementsByClassName('block');
+
+for(var i = 1; i < 400; i++){
+    bg.innerHTML += "<div class='block'></div>";
+    const duration = Math.random() * 5;
+    blocks[i].style.animationDuration = 2+duration+'s';
+    blocks[i].style.animationDelay = duration+'s';
+}
+
+const container = document.querySelector('.container');
+setTimeout(function(){
+    container.classList.add('active');
+}, 14000);
